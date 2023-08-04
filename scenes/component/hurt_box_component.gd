baa9c4fab40b6e76542a8225c78a8aeb15c161a3
Cuauhtemoc Ayala <vampiresqu1d@tutanota.com>
2023-08-04 02:10:00 -0700

    added hit flash shader, themed font, and damage text

--- scenes/component/hurt_box_component.gd
+++ scenes/component/hurt_box_component.gd
@@ -3,6 +3,8 @@ class_name HurtboxComponent
 
 @export var health_component: Node
 
+var floating_text_scene = preload("res://scenes/ui/floating_text.tscn")
+
 
 func _ready():
 	area_entered.connect(on_area_entered)
@@ -18,4 +20,10 @@ func on_area_entered(other_area: Area2D):
 	
 	var hitbox_component = other_area as HitboxComponent
 	health_component.damage(hitbox_component.damage)
+	
+	var floating_text = floating_text_scene.instantiate() as Node2D
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text)
+	
+	floating_text.global_position = global_position + (Vector2.UP * 16)
+	floating_text.start(str(hitbox_component.damage))
 	
