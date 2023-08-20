8966814b7fcce9c718e50e748d3561c55b1cfb7d
Cuauhtemoc Ayala <vampiresqu1d@tutanota.com>
2023-08-19 18:05:27 -0700

    added main menu, options menu, and pause menu, all with styles.

---
+++ scenes/ui/main_menu.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+var options_scene = preload("res://scenes/ui/options_menu.tscn")
+
+func _ready():
+	$%PlayButton.pressed.connect(on_play_pressed)
+	$%OptionsButton.pressed.connect(on_options_pressed)
+	$%QuitButton.pressed.connect(on_quit_pressed)
+	
+
+
+func on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+
+
+func on_options_pressed():
+	var options_instance = options_scene.instantiate()
+	add_child(options_instance)
+	options_instance.back_pressed.connect(on_options_closed.bind(options_instance))
+	
+
+
+func on_options_closed(options_intance: Node):
+	options_intance.queue_free()
+
+
+func on_quit_pressed():
+	get_tree().quit()
